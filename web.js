7eda17391b99d8dab7e1634cec20ecb4d58e25e9
Hack Reactor Students <github@hackreactor.com>
2013-05-03 12:31:32 -0700

    Setting up project for mongoose, handlebars, and everyauth

--- web.js
+++ web.js
@@ -3,33 +3,45 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path'),
+    everyauth = require('./node_modules/everyauth'),
+    exphbs  = require('express3-handlebars'),
+    mongoose = require('express-mongoose');
 
 var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 
 // all environments
 app.set('port', process.env.PORT || 5000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'handlebars');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Add everyauth middleware
+app.use(everyauth.middleware());
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', function (req, res, next) {
+  res.render('home');
+});
+
 app.get('/users', user.list);
+// everyauth.helpExpress(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
