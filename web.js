75cbf1f31517324e4769030aa59f80e63b1975b6
Hack Reactor Students <github@hackreactor.com>
2013-05-03 15:12:00 -0700

    working post request for login form

--- web.js
+++ web.js
@@ -10,26 +10,55 @@ var express = require('express'),
     path = require('path'),
     everyauth = require('./node_modules/everyauth'),
     exphbs  = require('express3-handlebars'),
-    mongoose = require('express-mongoose');
+    mongoose = require('mongoose');
 
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 
+/* Every Auth test code */
+everyauth.everymodule.findUserById(function() {
+  console.log('everyauth');
+});
+
+everyauth.password
+  .getLoginPath('/login')
+  .postLoginPath('/login')
+  .loginView('login')
+  .authenticate(function(login, password) {
+    console.log('authenticate called');
+  })
+  .loginSuccessRedirect('/')
+  .getRegisterPath('/register')
+  .postRegisterPath('/register')
+  .registerView('register')
+  .validateRegistration(function(newUserAttributes) {
+    console.log('validate registration');
+  })
+  .registerUser(function(newUserAttributes) {
+    console.log('register new user');
+  })
+  .registerSuccessRedirect('/');
+
+// everyauth.password.
+
 // all environments
 app.set('port', process.env.PORT || 5000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
+app.use(express.cookieParser());
+
+// Add everyauth middleware
+app.set(express.session({secret: 'secret'}));
+app.use(everyauth.middleware());
+
+app.use(express.favicon());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Add everyauth middleware
-app.use(everyauth.middleware());
 
 // development only
 if ('development' == app.get('env')) {
@@ -37,6 +66,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', function (req, res, next) {
+  console.log('user: ', req.user);
   res.render('home');
 });
 
