5a482f269742efd0d10dc9e297a372d85a8bb870
Hack Reactor Students <github@hackreactor.com>
2013-05-06 16:54:51 -0700

    (wip)

--- routes/index.js
+++ routes/index.js
@@ -34,23 +34,24 @@ exports.sendSMS = function(req, res){
 };
 
 exports.retrieveSMS = function(req, res) {
-    var accountSid = 'AC5933d34eda950c0bb81ed94811a9c13c';
-    var authToken = "99143cc9267d4ad6db22cdc12856ad5a";
-    var client = require('twilio')(accountSid, authToken);
-    client.sms.messages.list(function(err, data) {
-      var parsedData = JSON.parse(data.sms_messages);
-      // console.log(parsedData);
-      var messageInfo = [];
-      _.each(parsedData, function(item) {
-        messageData = {
-          phone: item.from,
-          sid : item.sid,
-          body: item.body,
-          date: item.date_created
-        };
-        messageInfo.push(messageData);
-      });
-      console.log(messageInfo);
+  console.log('retrieving SMS');
+  var accountSid = 'AC5933d34eda950c0bb81ed94811a9c13c';
+  var authToken = "99143cc9267d4ad6db22cdc12856ad5a";
+  var client = require('twilio')(accountSid, authToken);
+  client.sms.messages.list(function(err, data) {
+    var parsedData = JSON.parse(data.sms_messages);
+    console.log(parsedData);
+    var messageInfo = [];
+    _.each(parsedData, function(item) {
+      messageData = {
+        phone: item.from,
+        sid : item.sid,
+        body: item.body,
+        date: item.date_created
+      };
+      messageInfo.push(messageData);
     });
+    console.log(messageInfo);
+  });
 };
 
