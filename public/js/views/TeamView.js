96a6bae3361eb1c92394b02842d65999be18eed6
Hack Reactor Students <github@hackreactor.com>
2013-05-13 12:35:07 -0700

    Styling team view

--- public/js/views/TeamView.js
+++ public/js/views/TeamView.js
@@ -1,18 +1,46 @@
 var TeamView = Marionette.ItemView.extend({
   model: Team,
   template: '#teamView-template',
+  className: 'team',
 
   events: {
-    'click .team': 'showDetails'
+    'click .name': 'showDetails',
+    'click #createSms': 'showSmsForm',
+    'click #sendSms': 'sendSms',
+    'click #cancelSms': 'cancelSms'
+  },
+
+  initialize: function() {
+    // this.
+  },
+
+  showSmsForm: function(e) {
+    this.$('#createSms').hide();
+    this.$('.details').slideDown(500);
+    this.$('.sms-form').fadeIn(500);
+  },
+
+  hideSmsForm: function() {
+    this.$('.sms-form').fadeOut(500);
+    this.$('#createSms').show();
+  },
+
+  sendSms: function(e) {
+    this.hideSmsForm();
+  },
+
+  cancelSms: function(e) {
+    this.hideSmsForm();
   },
 
   showDetails: function(e) {
-    // console.log()
+    console.log('show details');
     this.$('.details').slideToggle(500);
   },
 
   onRender: function() {
     // Hide the details
     this.$('.details').hide();
+    this.$('.sms-form').hide();
   }
 });
