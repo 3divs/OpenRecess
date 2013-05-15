8277bd0b28fe518ad2e84cbd559a9667e13d2c2f
Mark Lee <marklee22@gmail.com>
2013-05-14 17:12:14 -0700

    Adding heroku specific configs

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ var config = {};
 
 config.db = {};
 
-config.db = process.env.DBCONN || 'mongodb://localhost:17017/openRecess';
+config.db = process.env['MONGOLAB_URI'];  //|| 'mongodb://localhost:17017/openRecess';
 config.port = process.env.PORT || 5000;
 
 module.exports = config;
