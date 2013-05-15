3065ecf31601e6d6d176277b71f5ee5a0b2a690a
Andrew Magliozzi <andrewmagliozzi@Andrew-Magliozzis-MacBook-Air.local>
2013-05-15 15:03:47 -0400

    moved twilio auth and secret keys into bash profile and heroku settings

--- config/config.js
+++ config/config.js
@@ -5,4 +5,8 @@ config.db = {};
 config.db = process.env.DBCONN || 'mongodb://localhost:17017/openRecess';
 config.port = process.env.PORT || 5000;
 
+config.twilioSID = process.env.TWILIO_ACCOUNT_SID;
+config.twilioAuth = process.env.TWILIO_AUTH_TOKEN;
+config.twilioNumber = '+14248887537';
+
 module.exports = config;
